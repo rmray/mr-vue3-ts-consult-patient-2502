74b22dd308887472b3ac9014a1ea4f218716286f
Ray <1497161283@qq.com>
2025-03-03 16:14:09 +0800

    环境搭建-目录结构

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-// const name = 'abc'
-
-a = "ccc"
-
-console.log('test3')
 
 const app = createApp(App)
 
