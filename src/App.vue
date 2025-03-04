0e9f8a42396d5f80590b7c5866adf84b50f9fc7c
Ray <1497161283@qq.com>
2025-03-04 10:48:43 +0800

    build(环境): store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from './store'
+
+const userStore = useUserStore()
+
+function hdlSetUser() {
+  userStore.setUser({
+    token: 'token',
+    id: '123',
+    account: 'tom',
+    mobile: '13222221111',
+    avatar: 'head'
+  })
+}
+function hdlDelUser() {
+  userStore.delUser()
+}
+</script>
 
 <template>
   <div class="app">
-    App
-    <van-button type="primary" size="small">主要按钮</van-button>
+    <van-button @click="hdlSetUser" size="small">添加用户</van-button>
+    <van-button @click="hdlDelUser" size="small">删除用户</van-button>
+    <hr />
+    {{ userStore.user }}
+    <hr />
   </div>
 </template>
 
